ff336aa6cb1f48ffc2a3e35ebf80299f2ba7814c
Gabriel Souza Lima <gabriel.lima607@etec.sp.gov.br>
2025-08-11 15:16:25 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./screens/HomeScreen";
+import AboutScreen from "./screens/AboutScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Início" }}
+        />
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
+          options={{ title: "Sobre" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
